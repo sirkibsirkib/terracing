01262ee2f5088864ada90a2b78fce74b729860bf
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2020-06-04 10:56:51 +0200

    more

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# terracing" 
+# Terracing
+Using 2D simplex noise to generate inked-style topographical maps (I'm thinking maps and levels for some video game).
+Important self-imposed requirement: each pixel is 100% independently sampled.
+
+Generation of one cell involves distinct steps:
+1. determine height from a set of 1st order and 2nd order noise fields (2nd order: field is sampled from output of another sample)
+1. terracing. heights are clamped at regular intervals
+1. regions of terraced land are pushed up and down to break up contiguous regions of terrain.
+1. terrace boundaries are turned into cliffs (Drawn dark), except for some regions ("ramps").
+1. a water table is generated from a noise field, and submerged regions are drawn underwater.
+
+![Example Image](https://github.com/sirkibsirkib/terracing/tree/master/example.png)
--- src/main.rs
+++ src/main.rs
@@ -38,35 +38,41 @@ fn frac_to_byte(x: f64) -> u8 {
     (x * 256.) as u8
 }
 
+#[inline]
+fn normalize(x: f64) -> f64 {
+    (x * 0.5 * 1.9) + 0.5
+}
+
 static mut MAX: f64 = -0.999;
 fn exp_sample<'a>(
     fields: impl Iterator<Item = &'a Field> + 'a,
     [x, y]: [f64; 2],
     mut scalar: f64, // doubles
 ) -> f64 {
-    const FACTOR: f64 = 2.;
+    const FACTOR: f64 = 2.1;
     // invariant: scalar.recip() == scalar_recip
     let mut scalar_recip = scalar.recip(); // halves.
     let mut sample = 0.;
-    // invariant: -1 <= (sample / sample_unnorm) <= 1
-    let mut sample_unnorm = 0.;
+    // invariant: -1 <= (sample / mass) <= 1
+    let mut mass = 0.;
     for field in fields {
         let v = field.get([x * scalar, y * scalar]);
 
         sample += v * scalar_recip * 1.;
-        sample_unnorm += scalar_recip;
+        mass += scalar_recip;
 
         scalar *= FACTOR;
         scalar_recip /= FACTOR;
     }
+    sample / mass
 
-    sample *= 1.9 / sample_unnorm;
+    // sample *= 1.9 / mass;
 
-    // more_asserts::assert_le!(-1., sample);
-    // more_asserts::assert_le!(sample, 1.);
-    unsafe { MAX = MAX.max(sample) }
+    // // more_asserts::assert_le!(-1., sample);
+    // // more_asserts::assert_le!(sample, 1.);
+    // unsafe { MAX = MAX.max(sample) }
 
-    sample
+    // sample
 }
 
 fn _field_test_max(field: &Field) -> f64 {
@@ -82,16 +88,18 @@ fn _field_test_max(field: &Field) -> f64 {
 
 fn main() {
     const TOT_OCTAVES: usize = 32;
+    const GROUND_OCTAVES: usize = 6;
+    const WATER_OCTAVES: usize = 2;
+    const TERRACE_OCTAVES: usize = 4;
+
     const GROUND_OCTAVE_OFFSET: usize = 0;
-    const GROUND_OCTAVES: usize = 7;
-    const TERRACE_OCTAVE_OFFSET: usize = 3;
-    const TERRACE_OCTAVES: usize = 6;
-    const WATER_OCTAVE_OFFSET: usize = 5;
+    const TERRACE_OCTAVE_OFFSET: usize = 2;
+    const WATER_OCTAVE_OFFSET: usize = 4;
     const RAMP_OCTAVE_OFFSET: usize = 6;
 
-    const SCALAR_C: f64 = 2.7;
-    const TERRACES: f64 = 26.;
-    const RAMP_PROP: f64 = 0.14;
+    const SCALAR_C: f64 = 4.;
+    const TERRACES: f64 = 21.;
+    const RAMP_PROP: f64 = 0.15;
 
     let fields: Vec<Field> = (0..)
         .take(TOT_OCTAVES)
@@ -99,7 +107,7 @@ fn main() {
         .collect();
 
     use rayon::iter::{IntoParallelIterator as _, ParallelIterator as _};
-    (0usize..16).into_par_iter().for_each(|var| {
+    (8usize..16).into_par_iter().for_each(|var| {
         let mut iw_ground = ImgWriter::new(&format!("images/image_{}a_ground.png", var));
         let mut iw_terrace = ImgWriter::new(&format!("images/image_{}b_terraced.png", var));
         let mut iw_water = ImgWriter::new(&format!("images/image_{}c_water.png", var));
@@ -111,12 +119,41 @@ fn main() {
                 let p = fields.iter().cycle().skip(var);
 
                 let [x, y] = noise_pt([xi, yi]);
-                let ground = exp_sample(
-                    p.clone().skip(GROUND_OCTAVE_OFFSET).take(GROUND_OCTAVES),
-                    [x, y],
-                    SCALAR_C,
-                ) * 0.5
-                    + 0.5;
+                let ground = {
+                    let raw = normalize(exp_sample(
+                        p.clone().skip(GROUND_OCTAVE_OFFSET).take(GROUND_OCTAVES),
+                        [x, y],
+                        SCALAR_C,
+                    ));
+                    let sample = |offset, scalar| {
+                        normalize(
+                            p.clone()
+                                .nth(GROUND_OCTAVE_OFFSET + GROUND_OCTAVES + offset)
+                                .unwrap()
+                                .get([y * scalar, x * scalar]),
+                        )
+                    };
+                    let mut samples = [
+                        sample(07, SCALAR_C * 00.4),
+                        sample(08, SCALAR_C * 00.7),
+                        sample(09, SCALAR_C * 01.09),
+                        sample(10, SCALAR_C * 01.80),
+                        sample(11, SCALAR_C * 01.99),
+                        sample(11, SCALAR_C * 2.045),
+                        sample(12, SCALAR_C * raw),
+                    ];
+                    samples.sort_by(|a, b| a.partial_cmp(b).unwrap());
+                    let (lower_idx, upper_contrib) = match raw {
+                        x if x < 0.2 => (0, (x - 0.0) / 0.2),
+                        x if x < 0.4 => (1, (x - 0.2) / 0.2),
+                        x if x < 0.6 => (2, (x - 0.4) / 0.2),
+                        x if x < 0.8 => (3, (x - 0.6) / 0.2),
+                        x => (4, (x - 0.8) / 0.2),
+                    };
+                    assert!(0. <= upper_contrib && upper_contrib <= 1.);
+                    samples[lower_idx] * (1. - upper_contrib)
+                        + samples[lower_idx + 1] * upper_contrib
+                };
                 let ground_byte = frac_to_byte(ground);
 
                 let (rawterrace, terrace, is_ramp) = {
@@ -131,13 +168,13 @@ fn main() {
                     };
                     let level_is_even = level as u32 % 2 == 0;
                     {
-                        let mut sample = exp_sample(
+                        let mut sample = normalize(exp_sample(
                             p.clone().skip(TERRACE_OCTAVE_OFFSET).take(TERRACE_OCTAVES),
                             [x, y],
                             -SCALAR_C * 2.,
-                        );
-                        const INC_WHEN_OVER: f64 = 0.2;
-                        const CLOSE_WHEN_OVER: f64 = INC_WHEN_OVER * (1. - RAMP_PROP * 0.65);
+                        ));
+                        const INC_WHEN_OVER: f64 = 0.6;
+                        const CLOSE_WHEN_OVER: f64 = 0.582;
                         if level_is_even {
                             sample = -sample;
                         }
@@ -156,11 +193,13 @@ fn main() {
                 let terrace_byte = frac_to_byte(terrace);
                 let rawterrace_byte = frac_to_byte(rawterrace);
 
-                let water_z = 0.8;
+                // let water_z = 0.8;
                 let water = {
-                    //
-                    let pt = [x * -0.7, y * -0.7, water_z];
-                    p.clone().nth(WATER_OCTAVE_OFFSET).unwrap().get(pt) * 0.4 + 0.5
+                    normalize(exp_sample(
+                        p.clone().skip(WATER_OCTAVE_OFFSET).take(WATER_OCTAVES),
+                        [x, y],
+                        SCALAR_C * 0.25,
+                    ))
                 };
                 let [water_rg_byte, water_b_byte, ramp_rg_byte, ramp_b_byte] = {
                     if ground < water {
@@ -176,7 +215,9 @@ fn main() {
                             if is_ramp {
                                 // this is a cliff
                                 let pt = [x * 20. * SCALAR_C, y * 20. * SCALAR_C];
-                                if p.clone().nth(RAMP_OCTAVE_OFFSET).unwrap().get(pt) > 0.25 {
+                                if normalize(p.clone().nth(RAMP_OCTAVE_OFFSET).unwrap().get(pt))
+                                    > 0.65
+                                {
                                     // ramp
                                     ground_byte
                                 } else {
